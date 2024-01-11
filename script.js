cc304475426d7d0cdf135721836c30d0e80a435f
ebaybasics <kccleancarpets@gmail.com>
2024-01-11 09:23:06 -0600

    Add FizBuzz Challenge to JS

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ const headline = document.querySelector('.headline');
 headline.addEventListener('click', e => {
     console.log('Clicked');
 })
+
+let numb = parseInt(prompt('Choose a number'));
+for (let i = 0; i < numb; i++) {
+    if (i === 0 || i < 0) {
+        continue;
+    }
+    if (i % 15 === 0) {
+        console.log('FizzBuzz');
+    } else if (i % 5 === 0) {
+        console.log('Buzz');
+    } else if (i % 3 === 0) {
+        console.log('Fizz');
+    } else console.log(i);
+    
+}
